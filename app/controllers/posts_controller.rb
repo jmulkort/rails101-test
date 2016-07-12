3d72466699a284ce588dbe5b37d4c11eea0f39c3
陳樂樂 <jmulkort@gmail.com>
2016-07-12 21:37:55 +0800

    Step 7 : 在 group 裡面可以刪除文章

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,11 @@ class PostsController < ApplicationController
   	@post = @group.posts.new
   end
 
+  def edit
+  	@group = Group.find(params[:group_id])
+  	@post = @group.posts.find(params[:id])
+  end
+
 def create
 	@group = Group.find(params[:group_id])
 	@post = @group.posts.build(post_params)
@@ -13,6 +18,25 @@ def create
 	else
 		render :new
 	end
+end
+
+ def update
+  @group = Group.find(params[:group_id])
+  @post = @group.posts.find(params[:id])
+
+  if @post.update(post_params)
+  	redirect_to group_path(@group), notice: "文章修改成功！"
+  else
+    render :edit
+  end
+end
+
+def destroy
+  @group = Group.find(params[:group_id])
+  @post = @group.posts.find(params[:id])
+
+  @post.destroy
+  redirect_to group_path(@group), alert: "文章已刪除"
 end
 
 private
